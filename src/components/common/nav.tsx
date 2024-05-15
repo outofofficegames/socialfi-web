276a0ebfacb1f296b851020b7c5af65b0de861a6
ogous <oguzxan.ali@gmail.com>
2024-05-15 17:53:21 +0300

    feat: add active indicator to nav

--- src/components/common/nav.tsx
+++ src/components/common/nav.tsx
@@ -1,37 +1,44 @@
+'use client'
 import clsx from 'clsx'
 import Link from 'next/link'
 import LeaderIcon from '#/leader-icon.png'
 import MissonIcon from '#/mission-icon.png'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 const navLinks = [
   {
     title: 'Missions',
     href: '/',
-    color: 'text-BDlightblue',
     image: MissonIcon,
   },
   {
     title: 'Leaders',
     href: '/leaders',
-    color: 'text-BDpink',
     image: LeaderIcon,
   },
 ]
 
 export default function Nav() {
+  const pathname = usePathname()
   return (
     <nav className="flex gap-10">
       {navLinks.map((item) => (
         <Link
           key={item.href}
-          className={clsx(
-            'uppercase text-2xl flex items-center gap-4',
-            item.color
-          )}
           href={item.href}
+          className="flex items-center gap-4"
         >
           <Image src={item.image} alt={item.title} width={100} height={100} />
-          {item.title}
+          <span
+            className={clsx(
+              'uppercase text-2xl',
+              pathname === item.href
+                ? 'border-b-4 border-BDpink text-BDpink'
+                : ' text-BDlightblue'
+            )}
+          >
+            {item.title}
+          </span>
         </Link>
       ))}
     </nav>
