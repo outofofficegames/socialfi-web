d50428033f76e323c94b068c8632e205779bf757
ogous <oguzxan.ali@gmail.com>
2024-05-15 15:50:10 +0300

    style: add bg mask

--- src/components/seasonTracking/listItem.tsx
+++ src/components/seasonTracking/listItem.tsx
@@ -4,13 +4,13 @@ import BorderImage from '#/border-laser.png'
 
 export default function TrackingSeasonListItem({ item }: { item: any }) {
   return (
-    <div className="bg-cover bg-trackingSeasonItem bg-no-repeat gap-6 bg-layer1 flex border-4 border-BDlightblue items-center">
+    <div className="bg-cover bg-trackingSeasonItem bg-no-repeat gap-6 bg-layer1 flex border-4 border-BDlightblue items-center relative">
       <Image
         src={item.image}
         width={200}
         height={200}
         alt={item.title}
-        className="ml-6"
+        className="ml-6 z-10"
       />
       <div className="flex flex-col gap-4 z-10 py-6">
         <StrokedText var="h3" className="text-5xl">
@@ -36,6 +36,7 @@ export default function TrackingSeasonListItem({ item }: { item: any }) {
           Points
         </StrokedText>
       </div>
+      <div className="bg-gradient-to-r from-transparent via-layer1 to-layer1 absolute inset-0" />
     </div>
   )
 }
